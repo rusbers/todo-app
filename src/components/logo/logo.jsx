9b7347c995b178863992d09d6b626da63690a25e
Ruslan <berendeev.ruslan@gmail.com>
2023-08-04 22:37:15 +0100

    fix: logo

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -4,7 +4,7 @@ function Logo() {
   return (
     <a className='logo'>
       <img
-        src='/src/assets/icons/icon-logo.svg'
+        src='./icon-logo.svg'
         width='108'
         height='20'
         alt='lorem'
